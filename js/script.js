6f9b85c72328685870eb5d4febcaef76a6e3f0fd
MKRNaqeebi <mkrnaqeebi@gmail.com>
2017-02-24 00:25:33 +0500

    Front end
    Front end files

---
+++ js/script.js
@@ -0,0 +1,240 @@
+$(function() {
+    // Nav Tab stuff
+    $('.nav-tabs > li > a').click(function() {
+        if($(this).hasClass('disabled')) {
+            return false;
+        } else {
+            var linkIndex = $(this).parent().index() - 1;
+            $('.nav-tabs > li').each(function(index, item) {
+                $(this).attr('rel-index', index - linkIndex);
+            });
+        }
+    });
+    $('#step-1-next').click(function() {
+        // Check values here
+        var isValid = true;
+        
+        if(isValid) {
+            $('.nav-tabs > li:nth-of-type(2) > a').removeClass('disabled').click();
+        }
+    });
+    $('#step-2-next').click(function() {
+        // Check values here
+        var isValid = true;
+        
+        if(isValid) {
+            $('.nav-tabs > li:nth-of-type(3) > a').removeClass('disabled').click();
+        }
+    });
+    $('#step-3-next').click(function() {
+        // Check values here
+        var isValid = true;
+        
+        if(isValid) {
+            $('.nav-tabs > li:nth-of-type(4) > a').removeClass('disabled').click();
+        }
+    });
+});
+
+
+$(document).ready(function()
+{
+
+var settings = {
+	url: "upload.php",
+	method: "POST",
+	allowedTypes:"jpg,png,gif,doc,pdf,zip",
+	fileName: "myfile",
+	multiple: true,
+	
+	//dragdropWidth:800,
+	onSuccess:function(files,data,xhr)
+	{
+		$("#status").html("<font color='green'>Upload is success</font>");
+		
+	},
+	onError: function(files,status,errMsg)
+	{		
+		$("#status").html("<font color='red'>Upload is Failed</font>");
+	}
+}
+$("#mulitplefileuploader").uploadFile(settings);
+
+});
+	
+$("#startbutton").click(function()
+	{
+		extraObj.startUpload();
+		
+	}
+);
+
+
+
+
+
+
+
+
+
+jQuery.fn.extend({
+    listrap: function () {
+        var listrap = this;
+        listrap.getSelection = function () {
+            var selection = new Array();
+            listrap.children("li.active").each(function (ix, el) {
+                selection.push($(el)[0]);
+            });
+            return selection;
+        }
+        var toggle = "li .listrap-toggle ";
+        var selectionChanged = function() {
+            $(this).parent().parent().toggleClass("active");
+            listrap.trigger("selection-changed", [listrap.getSelection()]);
+        }
+        $(listrap).find(toggle + "img").on("click", selectionChanged);
+        $(listrap).find(toggle + "span").on("click", selectionChanged);
+        return listrap;
+    }
+});
+$(document).ready(function () {
+    $(".listrap").listrap().on("selection-changed", function (event, selection) {
+        console.log(selection);
+    });
+});
+
+
+
+
+
+
+
+
+
+// Starrr plugin (https://github.com/dobtco/starrr)
+var __slice = [].slice;
+
+(function($, window) {
+  var Starrr;
+
+  Starrr = (function() {
+    Starrr.prototype.defaults = {
+      rating: void 0,
+      numStars: 5,
+      change: function(e, value) {}
+    };
+
+    function Starrr($el, options) {
+      var i, _, _ref,
+        _this = this;
+
+      this.options = $.extend({}, this.defaults, options);
+      this.$el = $el;
+      _ref = this.defaults;
+      for (i in _ref) {
+        _ = _ref[i];
+        if (this.$el.data(i) != null) {
+          this.options[i] = this.$el.data(i);
+        }
+      }
+      this.createStars();
+      this.syncRating();
+      this.$el.on('mouseover.starrr', 'span', function(e) {
+        return _this.syncRating(_this.$el.find('span').index(e.currentTarget) + 1);
+      });
+      this.$el.on('mouseout.starrr', function() {
+        return _this.syncRating();
+      });
+      this.$el.on('click.starrr', 'span', function(e) {
+        return _this.setRating(_this.$el.find('span').index(e.currentTarget) + 1);
+      });
+      this.$el.on('starrr:change', this.options.change);
+    }
+
+    Starrr.prototype.createStars = function() {
+      var _i, _ref, _results;
+
+      _results = [];
+      for (_i = 1, _ref = this.options.numStars; 1 <= _ref ? _i <= _ref : _i >= _ref; 1 <= _ref ? _i++ : _i--) {
+        _results.push(this.$el.append("<span class='glyphicon .glyphicon-star-empty'></span>"));
+      }
+      return _results;
+    };
+
+    Starrr.prototype.setRating = function(rating) {
+      if (this.options.rating === rating) {
+        rating = void 0;
+      }
+      this.options.rating = rating;
+      this.syncRating();
+      return this.$el.trigger('starrr:change', rating);
+    };
+
+    Starrr.prototype.syncRating = function(rating) {
+      var i, _i, _j, _ref;
+
+      rating || (rating = this.options.rating);
+      if (rating) {
+        for (i = _i = 0, _ref = rating - 1; 0 <= _ref ? _i <= _ref : _i >= _ref; i = 0 <= _ref ? ++_i : --_i) {
+          this.$el.find('span').eq(i).removeClass('glyphicon-star-empty').addClass('glyphicon-star');
+        }
+      }
+      if (rating && rating < 5) {
+        for (i = _j = rating; rating <= 4 ? _j <= 4 : _j >= 4; i = rating <= 4 ? ++_j : --_j) {
+          this.$el.find('span').eq(i).removeClass('glyphicon-star').addClass('glyphicon-star-empty');
+        }
+      }
+      if (!rating) {
+        return this.$el.find('span').removeClass('glyphicon-star').addClass('glyphicon-star-empty');
+      }
+    };
+
+    return Starrr;
+
+  })();
+  return $.fn.extend({
+    starrr: function() {
+      var args, option;
+
+      option = arguments[0], args = 2 <= arguments.length ? __slice.call(arguments, 1) : [];
+      return this.each(function() {
+        var data;
+
+        data = $(this).data('star-rating');
+        if (!data) {
+          $(this).data('star-rating', (data = new Starrr($(this), option)));
+        }
+        if (typeof option === 'string') {
+          return data[option].apply(data, args);
+        }
+      });
+    }
+  });
+})(window.jQuery, window);
+
+$(function() {
+  return $(".starrr").starrr();
+});
+
+$( document ).ready(function() {
+      
+  $('#stars1').on('starrr:change', function(e, value){
+    $('#count1').html(value);
+  }); 
+  $('#stars2').on('starrr:change', function(e, value){
+    $('#count2').html(value);
+  }); 
+  $('#stars3').on('starrr:change', function(e, value){
+    $('#count3').html(value);
+  }); 
+  $('#stars4').on('starrr:change', function(e, value){
+    $('#count4').html(value);
+  }); 
+  $('#stars5').on('starrr:change', function(e, value){
+    $('#count5').html(value);
+  });
+  
+  $('#stars-existing').on('starrr:change', function(e, value){
+    $('#count-existing').html(value);
+  });
+});
